259e13b36f0bec434b7f87c18c69854039e844a4
Abhishek P <abhishekpclt1999@gmail.com>
2023-11-09 17:31:01 +0530

    changed allowed host for aws ip

--- ECHOTUNES/settings.py
+++ ECHOTUNES/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.127.74.170','0.0.0.0']
 
 INTERNAL_IPS = [
     "127.0.0.1",
