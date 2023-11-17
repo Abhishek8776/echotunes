a07e7bd9e9ba91a585ff2e9b3a0186790eb21ef4
Abhishek P <abhishekpclt1999@gmail.com>
2023-11-17 09:42:33 +0530

    updated git ignore

--- ECHOTUNES/settings.py
+++ ECHOTUNES/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
     "debug_toolbar",
-    # "storages",
     "s3_folder_storage",
     "corsheaders",
 ]
