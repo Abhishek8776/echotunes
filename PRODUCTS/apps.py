52e4094d6ba700bc85135f894ca6aee116410673
Abhishek P <abhishekpclt1999@gmail.com>
2023-11-24 07:44:49 +0530

    automatic run schedule_task when runserver

--- PRODUCTS/apps.py
+++ PRODUCTS/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.core.management import call_command
+import sys
 
 
 class ProductsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PRODUCTS'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "PRODUCTS"
+
+    def ready(self):
+        if "runserver" in sys.argv:
+            call_command("schedule_task")
